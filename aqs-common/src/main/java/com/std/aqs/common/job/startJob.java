fd64c770d97214e511551855c9e591e1c6a34ccf
xjq9426586 <xjq9426586@users.noreply.github.com>
2019-03-15 16:32:54 +0800

    。

--- aqs-common/src/main/java/com/std/aqs/common/job/startJob.java
+++ aqs-common/src/main/java/com/std/aqs/common/job/startJob.java
@@ -8,7 +8,7 @@ public class startJob implements Job{
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		System.out.println("123456");
+		System.out.println("job测试");
 		
 	}
 
